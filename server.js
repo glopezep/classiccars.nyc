e2ee28a0ab6bd6344e25aa9a5b822ecace2e0f85
glopezep <guillermolopez2529@gmail.com>
2016-06-27 17:32:24 -0400

    Agregado proyecto para mostrar

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express')
+
+var app = express()
+var port = 3000 || process.env.PORT
+
+app.use(express.static(`${__dirname}/public`))
+
+app.get('/', (req, res) => {
+  res.send(200)
+})
+
+app.listen(port, () => {
+  console.log(`Server listen on ${port}`)
+})
